40ffc83e29858ce0dc6a2d2157b58a11cb9da9a2
Rafi Mochamad <57250467+rafimchmd@users.noreply.github.com>
2022-10-04 16:27:08 +0700

    Init commit

---
+++ moloo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  moloo
+//
+//  Created by Rafi Mochamad Fahreza on 04/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
